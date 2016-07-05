ca263c9c021d999b4eb93f7f3c3d3d29e39e50e2
emilyp13 <elpair@gmail.com>
2016-07-05 13:50:23 -0400

    fixed hound violation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'coveralls'
 Coveralls.wear!('rails')
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
