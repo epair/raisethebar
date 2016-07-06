0ad370bbf8c97d3ebe6a47987a0d6963247c622c
emilyp13 <elpair@gmail.com>
2016-07-06 13:47:18 -0400

    fixed hound errors: long lines and single quotes

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -3,6 +3,5 @@ class Bar < ActiveRecord::Base
   validates :address, presence: true
   validates :state, presence: true
   validates :city, presence: true
-  validates :zip, presence: true, numericality: true, length: { is: 5}
-
+  validates :zip, presence: true, numericality: true, length: { is: 5 }
 end
