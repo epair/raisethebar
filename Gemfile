b7ecda51aa9eff1a491420af4c32b2b9e39ed0c0
emilyp13 <elpair@gmail.com>
2016-07-06 10:17:59 -0400

    wrote tests and CRUD actions for bar model

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'foundation-rails'
+gem 'faker'
 
 group :development, :test do
   gem 'capybara'
---
+++ app/controllers/bars_controller.rb
@@ -0,0 +1,50 @@
+class BarsController < ApplicationController
+  def index
+    @bars = Bar.all
+  end
+
+  def create
+    @bar = Bar.new(params_bar)
+    if @bar.save
+      flash[:notice] = "Bar successfully added!"
+      redirect_to bar_path(@bar)
+    else
+      flash[:error] = @bar.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def update
+    @bar = Bar.find(params[:id])
+    if @bar.update(params_bar)
+      flash[:success] = 'Bar successfully updated'
+      redirect_to bar_path(@bar)
+    else
+      flash[:errors] = @bar.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  def new
+    @bar = Bar.new
+  end
+
+  def show
+    @bar = Bar.find(params[:id])
+  end
+
+  def edit
+    @bar = Bar.find(params[:id])
+  end
+
+  def destroy
+    @bar = Bar.find(params[:id])
+    @bar.destroy
+    redirect_to bars_path
+  end
+
+  private
+  def params_bar
+    params.require(:bar).permit(:name, :address, :city, :state, :zip, :description)
+  end
+end
