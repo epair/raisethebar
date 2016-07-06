0fccf8eaea27ef847e448b30a38b0ce4f216f8a0
Frank Cardillo <facardillo91@gmail.com>
2016-07-06 14:08:55 -0400

    Merging master branch into our feature branch

---
+++ app/models/bar.rb
@@ -0,0 +1,7 @@
+class Bar < ActiveRecord::Base
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true, numericality: true, length: { is: 5 }
+end
