952b12425ff0df063f784e5fa340278a8c306a9a
Frank Cardillo <facardillo91@gmail.com>
2016-07-07 10:16:52 -0400

    Removed a pry statement

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
-  
+
   protected
 
   def sign_up(resource_name, resource)
   end
 
   def after_sign_up_path_for(resource)
-    binding.pry
     new_user_session_path
   end
 end
