3452df67020d456913007a47e14abb70d1748dd0
emilyp13 <elpair@gmail.com>
2016-07-08 11:43:28 -0400

    Implemented a login helper method to DRY tests Worked on by: Frank Cardillo, Mia Buckland, and Emily Pair

--- spec/features/user_deletes_bar_spec.rb
+++ spec/features/user_deletes_bar_spec.rb
@@ -9,11 +9,7 @@ feature 'user deletes bar' do
   let!(:bar){ FactoryGirl.create(:bar, user_id: user.id) }
 
   scenario 'user visits bar show page and deletes bar' do
-    visit new_user_session_path
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-
+    login_user(user)
     click_link bar.name
     click_link 'Delete'
 
@@ -24,10 +20,7 @@ feature 'user deletes bar' do
   end
   scenario 'user does not see delete link on bar detail page they did not create' do
     user2 = FactoryGirl.create(:user)
-    visit new_user_session_path
-    fill_in 'Username', with: user2.username
-    fill_in 'Password', with: user2.password
-    click_button 'Log in'
+    login_user(user2)
     click_link bar.name
 
     expect(page).to_not have_content('Delete')
--- spec/features/user_edits_bar_spec.rb
+++ spec/features/user_edits_bar_spec.rb
@@ -5,11 +5,7 @@ feature 'user edits bar' do
   let!(:bar){ FactoryGirl.create(:bar, user_id: user.id) }
 
   scenario 'user visits detail page and edits bar info successfully' do
-    visit new_user_session_path
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-
+    login_user(user)
     click_link bar.name
     click_link 'Edit'
 
@@ -25,11 +21,7 @@ feature 'user edits bar' do
   end
 
   scenario 'user visits detail page and edits bar info unsuccessfully' do
-    visit new_user_session_path
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-
+    login_user(user)
     visit bars_path
     click_link bar.name
     click_link 'Edit'
@@ -44,10 +36,7 @@ feature 'user edits bar' do
   end
   scenario 'user does not see edit link on bar detail page they did not create' do
     user2 = FactoryGirl.create(:user)
-    visit new_user_session_path
-    fill_in 'Username', with: user2.username
-    fill_in 'Password', with: user2.password
-    click_button 'Log in'
+    login_user(user2)
     click_link bar.name
 
     expect(page).to_not have_content('Edit')
