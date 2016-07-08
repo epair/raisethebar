6d5fca9e8da5c7a9dadd37f1fe3121709a179e56
emilyp13 <elpair@gmail.com>
2016-07-08 15:00:55 -0400

    Created tests and wrote code for editing a review Worked on by: Mia Buckland, Frank Cardillo, and Emily Pair

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :review do
+    sequence(:title) {|m| "Title #{m}"}
     sequence(:body) { |n| "body text weeeeee #{n}" }
     rating "1"
     price "3"
--- spec/features/reviews/user_views_reviews_spec.rb
+++ spec/features/reviews/user_views_reviews_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
 feature "user views reviews" do
+  let!(:user){ FactoryGirl.create(:user) }
   let!(:bar){ FactoryGirl.create(:bar, user_id: user.id) }
-  let!(:review1){ FactoryGirl.create(:review, bar_id: bar.id)}
+  let!(:review1){ FactoryGirl.create(:review, bar_id: bar.id, user_id: user.id)}
 
   scenario "user sees all reviews for the bar on the bar show page" do
-    visit bars_path
+    login_user(user)
     click_link bar.name
 
     expect(page).to have_content(review1.body)
