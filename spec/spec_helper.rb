2b369449fe0d9ccace431bb496ccaa502e63d5b8
Frank Cardillo <facardillo91@gmail.com>
2016-07-12 09:46:37 -0400

    Fixing more hound violations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,11 @@ require 'coveralls'
 Coveralls.wear!('rails')
 
 RSpec.configure do |config|
+
+  config.before :each do
+    ActionMailer::Base.deliveries.clear
+  end
+  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
