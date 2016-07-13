836b2a531165303db8ebd79a2dceac62dea8b08a
Frank Cardillo <facardillo91@gmail.com>
2016-07-13 13:01:30 -0400

    Configuring sendgrid. Worked on by Mia Buckland and Frank Cardillo.

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
- :user_name => 'app53572504@heroku.com',
- :password => 'SendGridCreds12345',
- :domain => 'https://intense-eyrie-92860.herokuapp.com',
- :address => 'smtp.sendgrid.net',
- :port => 587,
- :authentication => :plain,
- :enable_starttls_auto => true
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  ActionMailer::Base.smtp_settings = {
+   :user_name => 'app53572504@heroku.com',
+   :password => 'SendGridCreds12345',
+   :domain => 'https://intense-eyrie-92860.herokuapp.com',
+   :address => 'smtp.sendgrid.net',
+   :port => 587,
+   :authentication => :plain,
+   :enable_starttls_auto => true
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net', port: 587 }
 end
