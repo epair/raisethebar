c3223f3e85b467c15946287219c90f6c2a0c78b1
Frank Cardillo <facardillo91@gmail.com>
2016-07-13 15:17:49 -0400

    Fixing hound violations. Frank Cardillo, Mia Buckland

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
-  authentication: plain,
+  authentication: :plain,
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   domain: 'heroku.com',
