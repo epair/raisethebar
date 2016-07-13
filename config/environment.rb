a0a4fa49960357e0f3b5cd52badb02751c0b10bd
Frank Cardillo <facardillo91@gmail.com>
2016-07-13 11:28:16 -0400

    Configuring sendgrid. Worked on by Mia Buckland and Frank Cardillo.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+ :user_name => 'your_sendgrid_username',
+ :password => 'your_sendgrid_password',
+ :domain => 'yourdomain.com',
+ :address => 'smtp.sendgrid.net',
+ :port => 587,
+ :authentication => :plain,
+ :enable_starttls_auto => true
+}
