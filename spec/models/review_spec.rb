e2574939a757abf91c08aa53dec7d567dbb67ea0
Perry Fitz <perry.fitz@coloradocollege.edu>
2016-07-15 13:52:03 -0400

    added more model testing.
    Frank Cadillo, Perry Fitz

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -15,4 +15,16 @@ describe Review do
   it { should have_valid(:title).when('OMG') }
   it { should_not have_valid(:title).when(nil, '') }
 
+  describe "#vote_counter" do
+    it "returns the vote tally of upvotes - downvotes" do
+      review = Review.create(body: "blah", rating: 1, price: 1, title: "blah", user_id: 1, bar_id: 1, vote_count: 2)
+      vote1 = Vote.create(user_id: 1, review_id: review.id, upvote: true)
+      vote2 = Vote.create(user_id: 1, review_id: review.id, upvote: true)
+
+      vote_array = [vote1, vote2]
+
+      result = review.vote_counter(vote_array)
+      expect(result).to eq(2)
+    end
+  end
 end
